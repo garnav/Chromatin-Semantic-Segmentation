fab1078bf51cfe797e44762c94fc97c36ff131ac
Arnav Ghosh <ag983@cornell.edu>
2018-11-18 10:42:49 -0500

    Added BN

--- models.py
+++ models.py
@@ -3,7 +3,7 @@
 # 15th Nov. 2018
 
 from keras import backend as K
-from keras.layers import Conv1D, MaxPooling1D, UpSampling1D, Activation, Reshape, Input, Permute
+from keras.layers import Conv1D, MaxPooling1D, UpSampling1D, Activation, Reshape, Input, Permute, BatchNormalization
 from keras.models import Sequential, Model
 from keras.activations import softmax
 
@@ -13,32 +13,45 @@ def create_seg_net(num_classes, input_shape, init_kernel_size, mid_kernel_size, 
 
     # Encoder
     # module 1
-    model.add(Conv1D(init_num_filters, init_kernel_size, activation='relu', border_mode='same', name="conv1", input_shape=input_shape))
+    model.add(Conv1D(init_num_filters, init_kernel_size, border_mode='same', name="conv1", input_shape=input_shape))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
     model.add(MaxPooling1D(pool_length=2, border_mode='same', name='maxpool1'))
 
     # module 2
-    model.add(Conv1D(init_num_filters * 2, mid_kernel_size, activation='relu', border_mode='same', name="conv2"))
+    model.add(Conv1D(init_num_filters * 2, mid_kernel_size, border_mode='same', name="conv2"))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
     model.add(MaxPooling1D(pool_length=2, border_mode='same', name='maxpool2'))
 
     # module 3
-    model.add(Conv1D(init_num_filters * 4, mid_kernel_size, activation='relu', border_mode='same', name="conv3"))
+    model.add(Conv1D(init_num_filters * 4, mid_kernel_size, border_mode='same', name="conv3"))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
     model.add(MaxPooling1D(pool_length=2, border_mode='same', name='maxpool3'))
+
+    model.add(Conv1D(init_num_filters * 8, mid_kernel_size, border_mode='same', name="conv3"))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
 
     # Decoder (3 modules because pooling size is assumed to be 2)
     # module 1
     model.add(UpSampling1D(size=2, name='upsample1'))
-    model.add(Conv1D(init_num_filters * 4, mid_kernel_size, activation='relu', border_mode='same', name="post_up1_conv"))
+    model.add(Conv1D(init_num_filters * 4, mid_kernel_size, border_mode='same', name="post_up1_conv"))
+    model.add(BatchNormalization())
 
     # module 2
     model.add(UpSampling1D(size=2, name='upsample2'))
-    model.add(Conv1D(init_num_filters * 2, mid_kernel_size, activation='relu', border_mode='same', name="post_up2_conv"))
+    model.add(Conv1D(init_num_filters * 2, mid_kernel_size, border_mode='same', name="post_up2_conv"))
+    model.add(BatchNormalization())
 
     # module 3
     model.add(UpSampling1D(size=2, name='upsample3'))
-    model.add(Conv1D(init_num_filters, mid_kernel_size, activation='relu', border_mode='same', name="post_up3_conv"))
+    model.add(Conv1D(init_num_filters, mid_kernel_size, border_mode='same', name="post_up3_conv"))
+    model.add(BatchNormalization())
 
     # Pixel Classification #TODO check if activation necessary (paper uses it)
-    model.add(Conv1D(num_classes, 1, activation='relu',  border_mode='same', name="class_conv"))
+    model.add(Conv1D(num_classes, 1, border_mode='same', name="class_conv"))
 
     # TODO CHECK ALL AFTER THIS
     # Reshape in prep for softmax
